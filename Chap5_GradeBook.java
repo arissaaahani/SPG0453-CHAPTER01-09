e50ba25e646af16ac7b63f5b7b6c6f71b2c12728
arissaaahani <arissahani.abdulhadi@student.gmi.edu.my>
2024-11-15 00:10:02 +0800

    Add files via upload

---
+++ Chap5_GradeBook.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.pkgclass;
+
+/**
+ *
+ * @author 60135
+ */
+public class GradeBook {
+     public void displayMessage() {
+        System.out.println("Welcome to the Grade Book!");
+    }
+}
---
+++ Chap5_MainClass.java
@@ -0,0 +1,10 @@
+package main.pkgclass;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        GradeBook myGradeBook = new GradeBook();
+        myGradeBook.displayMessage();
+    }
+    
+}
