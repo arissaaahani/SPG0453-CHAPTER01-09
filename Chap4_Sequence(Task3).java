1b866a09e22738e750aadef009b7d509d3f8864c
arissaaahani <arissahani.abdulhadi@student.gmi.edu.my>
2024-11-14 17:45:27 +0800

    Add files via upload

---
+++ Chap4_Sequence(Task3).java
@@ -0,0 +1,12 @@
+package sequence;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Sequence number : ");
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+        }   
+    }   
+}
